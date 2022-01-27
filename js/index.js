ca8c9797fbe7f0c611de40745c89b0fbfe6b3708
Lavínia <laligesiane@gmail.com>
2022-01-27 17:39:45 -0300

    darkmode
    inicio

--- js/index.js
+++ js/index.js
@@ -1,11 +1,44 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
+const getStyle = (element, style) =>
+  window
+    .getComputedStyle(element)
+    .getPropertyValue(style)
+
+const initialColors = {
+  background: getStyle(html, "--background"),
+  bg: getStyle(html, "--bg"),
+  bgBox: getStyle(html, "--bg-box"),
+  colorHeadings: getStyle(html, "--color-headings"),
+  colorText: getStyle(html, "--color-text"),
+  white: getStyle(html, "--white"),
+  black: getStyle(html, "--black"),
+  brown: getStyle(html, "--brown"),
+  //pegar este estilo do CSS
+}
+
+const darkMode = {
+  background: "#000",
+  bg:"#333333",
+  bgBox: "#000",
+  colorHeadings: "#fff",
+  colorButton: "#777777",
+  colorText: "#B5B5B5",
+  white: "#dddddd",
+  black: "#fff",
+  brown: "#7777777",
+}
+
+const transformKey = key => "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+const changeColors = (colors) => {
+ Object.keys(colors).map(key =>
+  html.style.setProperty(transformKey(key), colors[key])
+  )
+}
+
+checkbox.addEventListener("change", ({target}) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
